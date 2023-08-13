bffcea4a1919e9ca2ba0800126dcac20e2362327
TomasDemeter <22tomdemeter22@gmail.com>
2023-08-13 12:58:40 +0200

    genomeranges

--- scripts/DESeq2.R
+++ scripts/DESeq2.R
@@ -1,28 +1,25 @@
 library(DESeq2)
 library(tidyverse)
 library(pheatmap)
+library(GenomicRanges)
+library(rtracklayer)
 
-
-args <- commandArgs(trailingOnly = TRUE)
+#args <- commandArgs(trailingOnly = TRUE)
 raw_counts_filepath <- "results/feature_counts_table.tsv" #args[1]
 meta_data_filepath <- "data/raw_reads/SRR_metadata.csv" #args[2]
-DESeq2_output <- args[3]
+gtf_filepath <- "config/refs/Mus_musculus.GRCm39.110.gtf"
+#DESeq2_output <- args[3]
 
-# load featureCounts output
-raw_counts <- read.csv(raw_counts_filepath, skip = 1, sep = "\t")
+# load featureCounts output, metadata adn gtf file
+raw_counts <- read.csv(raw_counts_filepath, skip = 1, sep = "\t", row.names="Geneid")
+meta_data <- read.csv(meta_data_filepath, row.names="SRR")
 
-# load metadata
-meta_data <- read.csv(meta_data_filepath)
-
-# set Geneid column as index
-row.names(raw_counts) <- raw_counts$Geneid
-raw_counts$Geneid <- NULL
 
 # Drop the columns 'Chr', 'Start', 'End', 'Strand', and 'Length'
-raw_counts <- raw_counts[, !(names(raw_counts) %in% c('Chr', 'Start', 'End', 'Strand', 'Length'))]
+raw_counts <- raw_counts[, !(colnames(raw_counts) %in% c('Chr', 'Start', 'End', 'Strand', 'Length'))]
 
 # Rename the remaining columns to keep only the part of the column name starting with 'SRR' (included) and ending with '_fastp_' (excluded)
-names(raw_counts) <- sapply(names(raw_counts), function(x) {
+colnames(raw_counts) <- sapply(colnames(raw_counts), function(x) {
   start_index <- regexpr('SRR', x)
   end_index <- regexpr('_fastp_', x)
   if (start_index != -1 & end_index != -1) {
@@ -32,20 +29,32 @@ names(raw_counts) <- sapply(names(raw_counts), function(x) {
   }
 })
 
-# keep only relevant columns and set them as factors
-meta_data <- meta_data[,c("SRR", "cell.type", "treatment")]
+# keep only relevant columns from metadata and set them as factors
+meta_data <- meta_data[,c("cell.type", "treatment")]
 meta_data$cell.type <- as.factor(meta_data$cell.type)
 meta_data$treatment <- as.factor(meta_data$treatment)
 
-# set SRR column as index
-row.names(meta_data) <- meta_data$SRR
-meta_data$SRR <- NULL
+# creating genomic ranges object
 
-#check that all the column names of featurecounts match the row names of metadata (both name and order)
-all(colnames(raw_counts) == rownames(meta_data))
+gr <- import(gtf_filepath)
 
 #create DESeq2 object
-dds <- DESeqDataSetFromMatrix(countData = raw_counts, colData = meta_data, design = ~ treatment)
+
+dds <- DESeqDataSetFromMatrix(countData = raw_counts,
+                              colData = meta_data,
+                              design = ~ treatment)
+
+
+gr_2 <- makeGRangesFromGTF(
+  gtf_filepath,
+  level = c("genes", "transcripts"),
+  ignoreVersion = TRUE,
+  extraMcols = TRUE
+)
+
+mcols(dds)
+
+rowRanges(dds) <- gr
 
 # keep only row that have more than 10 reads across all samples
 dds <- dds[rowSums(counts(dds)) >= 1, ]
@@ -69,7 +78,18 @@ mat <- mat - rowMeans(mat)
 pheatmap(log10(mat + 1))
 
 # Illustrate distance between silenced and control samples of each cell line with PCA
-plotPCA(vsd, intgroup = c("treatment"))
+plotPCA(vsd, intgroup = c("treatment", "cell.type"))
+
+
+
+
+
+
+
+
+fpkm_values <- fpkm(dds)
+
+
 
 # Test for significance in gene expression levels between cell lines using Wilcoxon signed-rank test
 wilcox.test(log10(mat + 1))
