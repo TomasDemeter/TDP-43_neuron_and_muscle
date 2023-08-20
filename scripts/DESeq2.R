c08b4ad8e36238736af3afcc982fa8a482631b6f
TomasDemeter <22tomdemeter22@gmail.com>
2023-08-20 13:43:18 +0200

    fixing problems with figures

--- scripts/DESeq2.R
+++ scripts/DESeq2.R
@@ -51,9 +51,9 @@ raw_counts <- read.csv(raw_counts_filepath, skip = 1, sep = "\t", row.names="Gen
 meta_data <- read.csv(meta_data_filepath, row.names="SRR")
 
 
-######################################
-# Preparing data for FPKM and DESeq2 #
-######################################
+#############################
+# Preparing data for DESeq2 #
+#############################
 
 # save gene length info for later fpmk calculcations
 gene_length <- raw_counts["Length"]
@@ -72,7 +72,10 @@ colnames(raw_counts) <- sapply(colnames(raw_counts), function(x) {
   }
 })
 
-# keep only relevant columns from metadata and set them as factors
+# rename values in treatment and keep only relevant columns from metadata and set them as factors
+meta_data$treatment <- ifelse(grepl("Control", meta_data$treatment, ignore.case = TRUE), "control", 
+                       ifelse(grepl("TDP", meta_data$treatment, ignore.case = TRUE), "TDP", meta_data$treatment))
+
 meta_data <- meta_data[,c("cell.type", "treatment")]
 meta_data$cell.type <- as.factor(meta_data$cell.type)
 meta_data$treatment <- as.factor(meta_data$treatment)
@@ -82,23 +85,26 @@ meta_data$treatment <- as.factor(meta_data$treatment)
 # DESeq2 analysis
 #################
 
-#create DESeq2 object
+# create DESeq2 object
 dds <- DESeqDataSetFromMatrix(countData = raw_counts,
                               colData = meta_data,
-                              design = ~ cell.type + treatment)
+                              design = ~ treatment)
 
+# specify factor level (what is treated and what is control)
+dds$treatment <- relevel(dds$treatment, ref = "control")
 
 # add gene_length info to dds object
 mcols(dds)$basepairs <- gene_length$Length
 
+# filter out genese with less than 1 read in all samples
 dds <- dds[which(rowSums(counts(dds)) >= 1),]
-
 
 # calculate fpkm
 fpkm_values <- fpkm(dds)
 
 # Perform PCA on the FPKM values
 pca_res <- prcomp(t(fpkm_values))
+
 
 # Create a data frame with the PCA results and sample information
 pca_df <- data.frame(PC1 = pca_res$x[,1], PC2 = pca_res$x[,2], treatment = colData(dds)$treatment, cell.type = colData(dds)$cell.type)
@@ -118,20 +124,18 @@ pca_plot_fpkm <- ggplot(pca_df, aes(x = PC1, y = PC2, color = group)) +
 print(pca_plot_fpkm)
 
 
+#################################################################
+#Expression levels of Tardbp in TDP-43-silenced C2C12 and NSC34 #
+#################################################################
 
-#____________________________________________________
-# Extract the FPKM values for the gene of interest
+# Extract the FPKM values for the gene of interest (mouse TDP-43)
 gene_fpkm <- fpkm_values["ENSMUSG00000041459",]
 
 # Create a data frame with the FPKM values, PCA2 scores, and sample information
 fpkm_pca_df <- data.frame(fpkm = gene_fpkm, pca2 = pca_res$x[,2], treatment = colData(dds)$treatment, cell.type = colData(dds)$cell.type)
 
-# Create a new variable that combines treatment and cell type
-fpkm_pca_df$group <- interaction(fpkm_pca_df$treatment, fpkm_pca_df$cell.type)
-
-log_gene_fpkm <- log10(gene_fpkm + 1)
-
 # Create a data frame with the log10(FPKM + 1) values and sample information
+log_gene_fpkm <- log10(gene_fpkm + 1)
 log_fpkm_df <- data.frame(sample = colnames(fpkm_values), log_fpkm = log_gene_fpkm, treatment = colData(dds)$treatment, cell.type = colData(dds)$cell.type)
 
 # Create a new variable that combines treatment and cell type
@@ -152,57 +156,22 @@ log_fpkm_plot <- ggplot(log_fpkm_df, aes(x = group, y = log_fpkm, color = group)
 print(log_fpkm_plot)
 
 
+###############################
+# visualise PCA2 and TDP FPKM #
+###############################
 
-#____________________
+# Create a new variable that combines treatment and cell type
+fpkm_pca_df$group <- interaction(fpkm_pca_df$treatment, fpkm_pca_df$cell.type)
 
-# specify factor level (what is treated and what is control)
-dds$treatment <- relevel(dds$treatment, ref = "siRNA Control (Luciferase)")
-
-# run DESeq2
-dds <- DESeq(dds)
-
-# Perform multiple testing adjustment using Benjamini and Hochberg's approach
-res <- results(dds, alpha = 0.05, pAdjustMethod = "BH")
-
-# Extract differentially expressed genes (padj < 0.05)
-degs <- subset(res, padj < 0.05)
-
-
-# plot gene expression dispersion
-plotDispEsts(dds)
-
-
-# Hierarchically cluster the differentially expressed genes based on log10(FPKM + 1)
-
-vsd <- vst(dds, blind = FALSE)
-#mat <- assay(vsd)[rownames(degs),]
-#mat <- mat - rowMeans(mat)
-#pheatmap(log10(mat + 1))
-
-
-# Illustrate distance between silenced and control samples of each cell line with PCA
-# generate the PCA plot
-
-
-# the NSC34 groups might be switched here!!!!
-pca_plot <- plotPCA(vsd, intgroup = c("treatment", "cell.type")) +
+log_fpkm_tdp_plot <- ggplot(fpkm_pca_df, aes(x = fpkm, y = pca2, color = group)) +
   geom_point(size = 5) +
-  xlab(paste0("PC1 (", round(summary(pca_res)$importance[2,1]*100), "%)")) +
+  ggtitle("") +
   ylab(paste0("PC2 (", round(summary(pca_res)$importance[2,2]*100), "%)")) +
-  coord_cartesian(ylim = c(-4, 4))# +
-  #scale_color_manual(values = c("#8a3838", "#f07f7e", "#51848a", "#99c0cc"), labels = c("C2C12 siLUC", "C2C12 siTDP", "NSC34 siLUC", "NSC34 siTDP")) +
-  # nature_theme()
+  xlab("Tardbp FPKM") +
+  scale_x_discrete(labels = c("siLUC", "siTDP", "siLUC", "siTDP")) +
+  scale_color_manual(values = c("#8a3838", "#f07f7e", "#51848a", "#99c0cc"),
+  labels = c("C2C12 siLUC", "C2C12 siTDP", "NSC34 siLUC", "NSC34 siTDP")) +
+  nature_theme()
 
-print(pca_plot)
+print(log_fpkm_tdp_plot)
 
-
-#____________
-
-# adjust the y-axis limits
-pca_plot <- pca_plot + coord_cartesian(ylim = c(-4, 4))
-# display the plot
-print(pca_plot)
-
-
-# save the output
-saveRDS(dds, file = DESeq2_output)
